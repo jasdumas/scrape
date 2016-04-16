22d27e193b86bf44603155c4603c8e3dae76ebc6
Jasmine Dumas <jasmine.dumas@gmail.com>
2016-04-16 08:10:58 -0400

    fixed weatherData package
    fixed weatherData package from github dev version
    
    added a k-means clustering go wine data from UCI repo in customer
    segmentation tab section
    
    updated ui with collapsible boxes and layout

--- beer-dashboard/server.R
+++ beer-dashboard/server.R
@@ -69,7 +69,7 @@ server <- function(input, output, session) {
   
   output$temp <- renderInfoBox({
     infoBox(
-      "Temperature", paste0(temp_reactive()), icon = icon("sun-o"),
+      "Temperature in Hartford", paste0(temp_reactive()), icon = icon("sun-o"),
       color = "orange"
     )
   })
@@ -79,6 +79,29 @@ server <- function(input, output, session) {
 #     a$TemperatureF
 #   })
 #   
+  #######################
+  ## segmentation tab ###  
+  ######################
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    red_wine[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$wine <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  
+  
+  
+  
   ##################
   ## Finance tab ###  
   ##################  
--- beer-dashboard/ui.R
+++ beer-dashboard/ui.R
@@ -25,28 +25,48 @@ dashboardPage(
       tabItem(tabName = "dashboard",
               fluidRow(
                 box(title = "CT Craft Beer Ranking via BeerAdvocate", status = "primary", solidHeader = TRUE, 
-                    leafletOutput("map")) #, 
-              ) #,
+                    leafletOutput("map")) , 
+                box(title = "Beer Box1", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                    plotOutput("box1"))
+              ),
+              fluidRow(
+                box(title = "Other box", status = "success", collapsible = TRUE, width = 12, 
+                    textOutput("demotext1"))
+              )
               
       ), 
-      tabItem(tabName = "customer"), 
+      tabItem(tabName = "customer", 
+              fluidRow(
+                box(title = "Wine Settings", status = "danger", width = 6, collapsible = TRUE,
+                    selectInput('xcol', 'X Variable', names(red_wine), selected = "ash"),
+                    selectInput('ycol', 'Y Variable', names(red_wine), selected = "hue"),
+                    numericInput('clusters', 'Cluster count', 3,
+                                 min = 1, max = 9)   ) ,
+                box(title = "Red Wine Clustering", status = "danger", width = 6, 
+                    plotOutput("wine")
+                )
+             # )
+              )  ),
       
       tabItem(tabName = "finance", 
               fluidRow(
-                box(title = "Controls", status = "info",
-                    textInput("symb", "Symbol", "BUD"),
-                    dateRangeInput("dates", 
-                                   "Date range",
-                                   start = "2015-01-01", 
-                                   end = as.character(Sys.Date())),
-                    checkboxInput("log", "Plot y axis on log scale", 
-                                  value = FALSE),
-                    checkboxInput("adjust", 
-                                  "Adjust prices for inflation", value = FALSE), 
-                    actionButton("go", "Go!")),
                 box(title = "Beer Stocks", status = "info", solidHeader = TRUE, 
                     plotOutput("plot"))
-              )), 
+              ), 
+              fluidRow(
+                box(title = "Controls", status = "info", collapsible = TRUE, collapsed = TRUE,
+                           textInput("symb", "Symbol", "BUD"),
+                           dateRangeInput("dates", 
+                                          "Date range",
+                                          start = "2015-01-01", 
+                                          end = as.character(Sys.Date())),
+                           checkboxInput("log", "Plot y axis on log scale", 
+                                         value = FALSE),
+                           checkboxInput("adjust", 
+                                         "Adjust prices for inflation", value = FALSE), 
+                           actionButton("go", "Go!"))
+                )
+              ), 
       
       tabItem(tabName = "environment", 
               fluidRow(
